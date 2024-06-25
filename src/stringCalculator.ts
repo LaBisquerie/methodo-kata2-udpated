f1a4df0e4130f3c5ec37480fa21f91125c76088d
Dindault Lucas <dindault.lucas@gmail.com>
2024-06-25 14:47:05 +0200

    Refacto add method to handle input ending with a separator

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,10 +3,15 @@ export function add(numbers: string): number {
     return 0;
   }
 
+  if (numbers.endsWith(',') || numbers.endsWith('\n')) {
+    throw new Error('Invalid input: separator at the end');
+  }
+
   if (/,\n|\n,/.test(numbers)) {
     throw new Error('Invalid input: consecutive separators');
   }
 
   const numArray = numbers.split(/[\n,]/);
+
   return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
