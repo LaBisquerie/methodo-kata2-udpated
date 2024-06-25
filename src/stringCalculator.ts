ca81b7a36e2c03225e920e6cb9106a4a67a208b0
Dindault Lucas <dindault.lucas@gmail.com>
2024-06-25 14:39:08 +0200

    Refacto add method to handle unknown number of arguments

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,9 +2,7 @@ export function add(numbers: string): number {
   if (numbers === '') {
     return 0;
   }
+
   const numArray = numbers.split(',');
-  if (numArray.length === 1) {
-    return parseInt(numbers);
-  }
   return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
