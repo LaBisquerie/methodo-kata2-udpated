b59273694cb4ca12ca03e7cda77c63e0f454ac25
Dindault Lucas <dindault.lucas@gmail.com>
2024-06-25 14:43:38 +0200

    Refacto add method to handle invalid input like '2,\\n3'

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,6 +2,11 @@ export function add(numbers: string): number {
   if (numbers === '') {
     return 0;
   }
+
+  if (/,\n|\n,/.test(numbers)) {
+    throw new Error('Invalid input: consecutive separators');
+  }
+
   const numArray = numbers.split(/[\n,]/);
   return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
